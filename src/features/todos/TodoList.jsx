ba332824e05f8c0d3786093123383088a594136f
Joun <urbanscratcher@gmail.com>
2023-11-23 14:00:57 +0900

    drag and drop

--- src/features/todos/TodoList.jsx
+++ src/features/todos/TodoList.jsx
@@ -1,17 +1,74 @@
+import { useRef } from "react";
 import TodoListItem from "./TodoListItem";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function TodoList({ todos, updateTodos }) {
+  console.log("[R] Todo List");
+
+  const [todoList, setTodoList] = useState(todos);
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+
+  useEffect(() => {
+    setTodoList(todos);
+  }, [todos]);
+
+  const updateListOnDragHandler = (idx) => {
+    const dragItemIdx = dragItem.current;
+    const newList = [...todos];
+    const draggedItem = todos[dragItemIdx];
+
+    // remove & insert
+    newList.splice(dragItemIdx, 1);
+    newList.splice(idx, 0, draggedItem);
+
+    // update presented list
+    setTodoList(newList);
+  };
+
+  const updateListHandler = (newList) => {
+    setTodoList(newList);
+    updateTodos(newList);
+  };
+
+  const commitListHandler = () => {
+    updateTodos(todoList);
+  };
+
+  const setDragItemHandler = (idx) => {
+    dragItem.current = idx;
+  };
+
+  const setDragOverItemHandler = (idx) => {
+    dragOverItem.current = idx;
+  };
+
+  const rollbackHandler = (e) => {
+    updateListHandler(todos);
+  };
+
+  const isDone = todos.length > 0 && todos.every((v) => v.done === true);
+
   return (
-    <ol className="text-3xl flex flex-col gap-5">
-      {todos.map((v, i) => (
-        <TodoListItem
-          key={v.id}
-          id={v.id}
-          idx={i}
-          updateTodos={updateTodos}
-          todo={v}
-        />
-      ))}
+    <ol className="text-3xl flex flex-col">
+      {!isDone &&
+        todoList.map((v, i) => (
+          <TodoListItem
+            olderIdx={todos.findIndex((todo) => todo.id === v.id)}
+            key={v.id}
+            id={v.id}
+            idx={i}
+            todo={v}
+            onUpdateList={updateListHandler}
+            onUpdateListOnDrag={updateListOnDragHandler}
+            onCommitList={commitListHandler}
+            onRollback={rollbackHandler}
+            onSetDragItem={setDragItemHandler}
+            onSetDragOverItem={setDragOverItemHandler}
+          />
+        ))}
+      {isDone && <p>done list</p>}
     </ol>
   );
 }
--- src/features/todos/TodoListItem.jsx
+++ src/features/todos/TodoListItem.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
 
-const TodoListItem = ({ id, updateTodos, idx, doneInit, todo }) => {
+const TodoListItem = ({
+  olderIdx,
+  idx,
+  id,
+  todo,
+  onUpdateList,
+  onUpdateListOnDrag,
+  onCommitList,
+  onRollback,
+  onSetDragItem,
+  onSetDragOverItem,
+}) => {
+  // console.log("[R] Todo List Item: ", idx);
   const [done, setDone] = useState(todo.done);
+  const [showDelete, setShowDelete] = useState(false);
+  const [dragging, setDragging] = useState(false);
+  const [dropped, setDropped] = useState(false);
 
+  // logic
   const deleteHandler = (e) => {
     setDone((done) => !done);
 
-    updateTodos((todos) =>
-      todos.map((todo, i) => {
+    onUpdateList((todoList) =>
+      todoList.map((todo, i) => {
         if (i === idx) {
           todo.done = !done;
         }
@@ -18,31 +34,100 @@ const TodoListItem = ({ id, updateTodos, idx, doneInit, todo }) => {
 
   const removeHandler = (e) => {
     e.stopPropagation();
-    updateTodos((todos) => todos.filter((_, i) => i !== idx));
+    onUpdateList((todoList) => todoList.filter((_, i) => i !== idx));
+  };
+
+  // mouse hover
+  const mouseEnterHandler = (e) => {
+    setShowDelete(true);
+  };
+
+  const mouseLeaveHandler = (e) => {
+    setShowDelete(false);
+  };
+
+  // drag & drop
+  const dragStartHandler = (e) => {
+    // prevent showing + sign
+    e.dataTransfer.effectAllowed = "move";
+
+    // set current dragged item
+    onSetDragItem(idx);
+
+    // for css styling of ghost element
+    setTimeout(() => {
+      // reset styling
+      setDragging(true);
+    }, 0);
+  };
+
+  const dragEnterHandler = (e) => {
+    e.preventDefault();
+
+    // let update happend in the parent list component
+    onUpdateListOnDrag(idx);
+  };
+
+  const dragEndHandler = (e) => {
+    e.preventDefault();
+
+    // when dropped in deviated area
+    !dropped && onRollback(e);
+
+    // initialize
+    dragging && setDragging(false);
+    dropped && setDropped(false);
+
+    onSetDragItem(null);
+    onSetDragOverItem(null);
+  };
+
+  const dropHandler = (e) => {
+    e.preventDefault();
+    onCommitList();
+    setDropped(true);
   };
 
   return (
-    <li key={id} flex justify-between px-10>
+    <li
+      key={id}
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+      onDragStart={dragStartHandler}
+      onDragEnter={dragEnterHandler}
+      onDragEnd={dragEndHandler}
+      onDrop={dropHandler}
+      onDragOver={(e) => e.preventDefault()}
+      onDragLeave={(e) => e.preventDefault()}
+      onDrag={(e) => e.preventDefault()}
+      draggable
+      className={`flex justify-between items-center cursor-pointer gap-6 pl-9 pr-9 py-3 rounded-lg ${
+        dragging && "bg-gray-50 text-transparent"
+      }`}
+    >
       <p
         onClick={(e) => deleteHandler(e)}
         cursor-pointer
         flex
         gap-4
+        flex-1
         className={`${
           done
             ? "line-through text-gray-200 hover:brightness-90"
             : "hover:text-black"
         }`}
       >
-        {`${idx + 1}. ${todo.text}`}
+        {`${olderIdx + 1}. ${todo.text}`}
       </p>
-      <div
-        className={`i-lucide:x ${
-          done ? "text-gray-200 hover:brightness-90" : "hover:text-black"
-        }`}
-        cursor-pointer
-        onClick={(e) => removeHandler(e)}
-      ></div>
+      {showDelete && (
+        <div
+          className={`i-lucide:x ${
+            done ? "text-gray-200 hover:brightness-90" : "hover:text-black"
+          }`}
+          cursor-pointer
+          onClick={(e) => removeHandler(e)}
+        ></div>
+      )}
     </li>
   );
 };
